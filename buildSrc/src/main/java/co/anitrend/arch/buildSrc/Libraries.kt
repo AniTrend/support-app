44921bcd885aae7b784d4b48234d81e813cef9f9
Maxwell <mxt.developer@gmail.com>
2021-05-06 16:07:20 +0200

    Bump material from 1.4.0-alpha01 to 1.4.0-alpha02

--- buildSrc/src/main/java/co/anitrend/arch/buildSrc/Libraries.kt
+++ buildSrc/src/main/java/co/anitrend/arch/buildSrc/Libraries.kt
@@ -96,7 +96,7 @@ object Libraries {
     }
 
     object Google {
-        private const val version = "1.4.0-alpha01"
+        private const val version = "1.4.0-alpha02"
         const val material = "com.google.android.material:material:$version"
     }
 
